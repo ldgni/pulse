0ca229c8344a46bb611fa355c05b687c21ded9a4
Luca Di Gianni <luca.dgni@gmail.com>
2024-03-27 12:10:12 +0100

    feat: add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="border-t border-slate-700 bg-secondary">
+      <div className="container flex justify-center py-4">
+        <p>
+          &copy; 2024{" "}
+          <a
+            href="https://ldgni.io/"
+            target="_blank"
+            className="text-secondary transition-colors hover:text-primary">
+            Luca Di Gianni
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
