aef6c8cb27a1dda9cc9dca0609e674d4269fc343
Luca Di Gianni <luca.dgni@gmail.com>
2024-05-27 15:14:34 +0200

    refactor: update component imports to match case sensitivity changes

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-import MatchList from "@/components/MatchList";
+import MatchList from "@/components/matchList";
 import { getFromAPI } from "@/services";
 
 export const metadata: Metadata = {
