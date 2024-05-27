90137f79189e9f8aba6d1ef7aa696a2d8d237adc
Luca Di Gianni <luca.dgni@gmail.com>
2024-05-27 14:48:12 +0200

    refactor: update component imports to match case sensitivity changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
