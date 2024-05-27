5644ad2a515f6c98665a9a5c1f1fabe3ff374c9d
Luca Di Gianni <luca.dgni@gmail.com>
2024-05-27 14:43:01 +0200

    refactor: header and remove navlink component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,56 @@
-import Image from "next/image";
+"use client";
 
-import NavLink from "@/components/ui/NavLink";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  {
+    href: "/fixtures",
+    label: "Fixtures",
+  },
+  {
+    href: "/results",
+    label: "Results",
+  },
+  {
+    href: "/standings",
+    label: "Standings",
+  },
+];
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className="sticky top-0 bg-primary">
-      <nav className="container max-w-screen-lg py-4">
-        <ul className="flex items-center justify-between gap-4 text-slate-400 sm:justify-normal sm:gap-12">
-          <li>
-            <NavLink href="/" className="flex gap-2">
-              <Image
-                src={`/images/psg-logo.webp`}
-                width={24}
-                height={24}
-                alt="Paris Saint-Germain logo"
-              />
-              <span className="hidden sm:block">Pulse</span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink href="/fixtures">Fixtures</NavLink>
-          </li>
-          <li>
-            <NavLink href="/results">Results</NavLink>
-          </li>
-          <li>
-            <NavLink href="/standings">Standings</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="container flex max-w-screen-lg gap-4 py-4 sm:gap-12">
+        <Link href="/" className="flex gap-2">
+          <Image
+            src="/images/psg-logo.webp"
+            className="size-6"
+            width={24}
+            height={24}
+            alt="Paris Saint-Germain logo"
+          />
+          <span className="hidden font-bold sm:block">Pulse</span>
+        </Link>
+        <nav>
+          <ul className="flex items-center justify-between gap-4 text-slate-400 sm:justify-normal sm:gap-12">
+            {navLinks.map((link) => (
+              <li key={link.href}>
+                <Link
+                  href={link.href}
+                  className={`${
+                    pathname === link.href ? "text-slate-100" : ""
+                  } font-medium transition-colors hover:text-slate-100`}>
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
