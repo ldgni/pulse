54e6a7a4578e621840b397a9d1db1694c1999058
Luca Di Gianni <luca.dgni@gmail.com>
2025-10-06 14:15:13 +0200

    fix: remove unnecessary transition

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -14,8 +14,8 @@ export function ModeToggle() {
       size="icon"
       title="Toggle theme"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-      <Sun className="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Sun className="scale-100 rotate-0 dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute scale-0 rotate-90 dark:scale-100 dark:rotate-0" />
     </Button>
   );
 }
