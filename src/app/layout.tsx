8e3d88b7c7797e7d7949bada1958c1ece78315e1
Luca Di Gianni <luca.dgni@gmail.com>
2025-10-07 18:37:18 +0200

    style: update layout padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           disableTransitionOnChange>
           <div className="mx-auto flex min-h-screen max-w-xl flex-col p-4">
             <Header />
-            <main className="flex-grow px-4">{children}</main>
+            <main className="flex-grow px-3">{children}</main>
             <Footer />
           </div>
         </ThemeProvider>
