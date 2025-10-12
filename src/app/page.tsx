706996f8b02d7cf7a48e9b9f7b5eb5dd257330d0
Luca Di Gianni <luca.dgni@gmail.com>
2025-10-12 18:12:04 +0200

    feat: implement homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,73 @@
-export default function HomePage() {
-  return <h1>Hello, World!</h1>;
+import Image from "next/image";
+
+import { MatchCard } from "@/components/match-card";
+import { Card, CardContent } from "@/components/ui/card";
+import { getFixtures, getResults, getStandings } from "@/lib/api";
+
+export default async function HomePage() {
+  const [results, fixtures, standings] = await Promise.all([
+    getResults(),
+    getFixtures(),
+    getStandings(),
+  ]);
+
+  const latestResult = results[0];
+  const nextFixture = fixtures[0];
+  const clubStanding = standings.find((team) => team.team.tla === "PSG");
+
+  return (
+    <>
+      <h1 className="mb-6 text-center text-2xl font-bold sm:text-3xl">Pulse</h1>
+      <div className="space-y-4">
+        {/* Latest result */}
+        <h2 className="text-xl font-semibold">Latest result</h2>
+        <MatchCard match={latestResult} type="result" />
+
+        {/* Next fixture */}
+        <h2 className="text-xl font-semibold">Next fixture</h2>
+        <MatchCard match={nextFixture} type="fixture" />
+
+        {/* League standing */}
+        {clubStanding && (
+          <>
+            <h2 className="text-xl font-semibold">Current ranking</h2>
+            <Card>
+              <CardContent>
+                <div className="flex justify-between">
+                  <div className="flex items-center gap-4">
+                    <span className="mr-2 text-2xl font-bold">
+                      {clubStanding.position}
+                    </span>
+                    <Image
+                      src={clubStanding.team.crest}
+                      alt={clubStanding.team.name}
+                      width={200}
+                      height={200}
+                      className="size-12"
+                    />
+                    <div>
+                      <span className="font-semibold sm:hidden">
+                        {clubStanding.team.tla}
+                      </span>
+                      <span className="hidden font-semibold sm:block">
+                        {clubStanding.team.name}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <span className="text-muted-foreground uppercase">
+                      Points
+                    </span>
+                    <div className="text-2xl font-bold">
+                      {clubStanding.points}
+                    </div>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
