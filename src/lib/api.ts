306cf84f3cd850ed629a6612a802d30283bcfb4e
Luca Di Gianni <luca.dgni@gmail.com>
2025-10-22 18:13:45 +0200

    refactor: improve error handling

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,14 @@
-import { Match, TeamStanding } from "@/types/api";
-
 const API_BASE_URL = "https://api.football-data.org/v4";
 const API_KEY = process.env.FOOTBALL_DATA_API_KEY || "";
 
+if (!API_KEY) {
+  throw new Error(
+    "FOOTBALL_DATA_API_KEY is not defined in environment variables",
+  );
+}
+
 async function fetchAPI(endpoint: string) {
-  const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+  const res = await fetch(`${API_BASE_URL}${endpoint}`, {
     headers: {
       "X-Auth-Token": API_KEY,
     },
@@ -13,20 +17,20 @@ async function fetchAPI(endpoint: string) {
     },
   });
 
-  return response.json();
+  return res.json();
 }
 
 export async function getStandings() {
   const { standings } = await fetchAPI("/competitions/FL1/standings");
-  return standings[0].table as TeamStanding[];
+  return standings[0].table;
 }
 
 export async function getFixtures() {
   const { matches } = await fetchAPI("/teams/524/matches?status=SCHEDULED");
-  return matches as Match[];
+  return matches;
 }
 
 export async function getResults() {
   const { matches } = await fetchAPI("/teams/524/matches?status=FINISHED");
-  return (matches as Match[]).toReversed();
+  return matches.toReversed();
 }
