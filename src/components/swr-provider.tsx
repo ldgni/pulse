b25263769a99432c275a9cd30328623aaa3adb1f
Luca Di Gianni <luca.dgni@gmail.com>
2025-10-23 14:50:26 +0200

    refactor: simplify fetcher function in swr-provider

--- src/components/swr-provider.tsx
+++ src/components/swr-provider.tsx
@@ -2,11 +2,7 @@
 
 import { SWRConfig } from "swr";
 
-const fetcher = async (url: string) => {
-  const res = await fetch(url);
-  if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
-  return res.json();
-};
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function SWRProvider({
   children,
