dd93e0b1252cce6bb46f0b98e5f31e27b51af317
Luca Di Gianni <luca.dgni@gmail.com>
2025-10-26 15:21:52 +0100

    feat: upgrade to nextjs 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,30 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import checkFile from "eslint-plugin-check-file";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  eslintConfigPrettier,
   {
     plugins: {
       "simple-import-sort": simpleImportSort,
-      "check-file": checkFile,
     },
     rules: {
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
-      "check-file/filename-naming-convention": [
-        "error",
-        {
-          "**/*": "KEBAB_CASE",
-        },
-        {
-          ignoreMiddleExtensions: true,
-        },
-      ],
-      "check-file/folder-naming-convention": [
-        "error",
-        {
-          "**/": "NEXT_JS_APP_ROUTER_CASE",
-        },
-      ],
     },
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
   },
-];
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
 
 export default eslintConfig;
