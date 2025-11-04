4322f1d5386dabafaaffdf12daaee579ddde7146
Luca Di Gianni <luca.dgni@gmail.com>
2025-11-04 11:53:23 +0100

    style: improve spacing

--- app/page.tsx
+++ app/page.tsx
@@ -13,28 +13,28 @@ export default function HomePage() {
         <h1 className="text-2xl font-bold sm:text-3xl">Welcome to Pulse</h1>
         <p className="text-muted-foreground">Paris Saint-Germain FC Tracker</p>
       </div>
-      <div className="space-y-4">
-        <>
-          <h2 className="text-xl font-semibold">Next fixture</h2>
+      <div className="space-y-8">
+        <div>
+          <h2 className="mb-4 text-xl font-semibold">Next fixture</h2>
           <Suspense fallback={<SkeletonCard count={1} />}>
             <NextFixture />
           </Suspense>
-        </>
-        <>
-          <h2 className="text-xl font-semibold">Previous result</h2>
+        </div>
+        <div>
+          <h2 className="mb-4 text-xl font-semibold">Previous result</h2>
           <Suspense fallback={<SkeletonCard count={1} />}>
             <PreviousResult />
           </Suspense>
-        </>
-        <>
-          <h2 className="text-xl font-semibold">Current ranking</h2>
+        </div>
+        <div>
+          <h2 className="mb-4 text-xl font-semibold">Current ranking</h2>
           <Suspense
             fallback={
               <Skeleton className="h-[106px] w-full rounded-xl border shadow-sm" />
             }>
             <CurrentRanking />
           </Suspense>
-        </>
+        </div>
       </div>
     </>
   );
