a2986d004e29fc7f4d5ebaa0b6e1818b54fb188d
Luca Di Gianni <luca.dgni@gmail.com>
2025-12-05 18:23:11 +0100

    fix: implement fix to solve hydration issue

--- components/header.tsx
+++ components/header.tsx
@@ -1,92 +1,14 @@
-"use client";
-
-import { Github, Menu } from "lucide-react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { Github } from "lucide-react";
 
 import ModeToggle from "@/components/mode-toggle";
+import NavLinks from "@/components/nav-links";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-
-const navLinks = [
-  {
-    href: "/",
-    label: "Home",
-  },
-  {
-    href: "/fixtures",
-    label: "Fixtures",
-  },
-  {
-    href: "/results",
-    label: "Results",
-  },
-  {
-    href: "/standings",
-    label: "Standings",
-  },
-];
 
 export default function Header() {
-  const pathname = usePathname();
-
   return (
     <header className="mb-8 flex items-center justify-between">
-      {/* Mobile Menu */}
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button
-            variant="ghost"
-            size="icon"
-            aria-label="Toggle menu"
-            className="sm:hidden">
-            <Menu />
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <SheetHeader>
-            <SheetTitle>Menu</SheetTitle>
-            <SheetDescription />
-          </SheetHeader>
-          <nav className="px-4">
-            <ul className="space-y-4">
-              {navLinks.map((link) => (
-                <li key={link.href}>
-                  <SheetClose asChild>
-                    <Link href={link.href}>{link.label}</Link>
-                  </SheetClose>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </SheetContent>
-      </Sheet>
-
-      {/* Desktop Menu */}
-      <nav className="hidden sm:block">
-        <ul className="flex gap-2">
-          {navLinks.map((link) => (
-            <li key={link.href}>
-              <Button
-                variant="ghost"
-                asChild
-                className={pathname === link.href ? "bg-accent" : ""}>
-                <Link href={link.href}>{link.label}</Link>
-              </Button>
-            </li>
-          ))}
-        </ul>
-      </nav>
-
+      <NavLinks />
       <div className="flex h-4 items-center gap-2">
         <Button variant="ghost" size="icon" asChild>
           <a
---
+++ components/nav-links.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const navItems = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/fixtures",
+    label: "Fixtures",
+  },
+  {
+    href: "/results",
+    label: "Results",
+  },
+  {
+    href: "/standings",
+    label: "Standings",
+  },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {/* Mobile Menu */}
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            aria-label="Toggle menu"
+            className="sm:hidden">
+            <Menu />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+            <SheetDescription />
+          </SheetHeader>
+          <nav className="px-4">
+            <ul className="space-y-4">
+              {navItems.map((item) => (
+                <li key={item.href}>
+                  <SheetClose asChild>
+                    <Link href={item.href}>{item.label}</Link>
+                  </SheetClose>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </SheetContent>
+      </Sheet>
+
+      {/* Desktop Menu */}
+      <ul className="hidden gap-2 sm:flex">
+        {navItems.map((item) => (
+          <li key={item.href}>
+            <Button
+              variant="ghost"
+              asChild
+              className={pathname === item.href ? "bg-accent" : ""}>
+              <Link href={item.href}>{item.label}</Link>
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
