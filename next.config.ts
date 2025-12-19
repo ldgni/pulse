63b015ccfd491fc6d38e6780be22a6e8840f38d9
Luca Di Gianni <luca.dgni@gmail.com>
2025-12-19 15:00:55 +0100

    fix: update remotePatterns in next.config to use structured object format for image loading

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("https://crests.football-data.org/**")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "crests.football-data.org",
+      },
+    ],
   },
 };
 
